cd3316f154d18432236dfb6cba7b15c34aba90ae
vli2014 <102990808+vli2014@users.noreply.github.com>
2025-04-20 12:50:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,14 @@ from config import TOKEN
 bot = telebot.TeleBot(TOKEN)
 
 # Обработчик команды '/start' и '/hello'
-@bot.message_handler(commands=['start', 'hello'])
+@bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-    bot.reply_to(message, f'Привет! Я бот {bot.get_me().first_name}!')
-
-# Обработчик команды '/heh'
-@bot.message_handler(commands=['heh'])
-def send_heh(message):
-    count_heh = int(message.text.split()[1]) if len(message.text.split()) > 1 else 5
-    bot.reply_to(message, "he" * count_heh)
+    bot.reply_to(message, """\
+Привет, я эхобот, я буду отвечать также как и ты!\
+""")
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
 
 # Запуск бота
-bot.polling()
+bot.infinity_polling()
