e9e3b16ecfc881e00622fe1cd8a4ee9cb4e7f5b1
vli2014 <102990808+vli2014@users.noreply.github.com>
2025-04-27 11:59:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
 import telebot
 from config import TOKEN
+import time
+import threading
+import schedule
 
 # Инициализация бота с использованием его токена
 bot = telebot.TeleBot(TOKEN)
 
-# Обработчик команды '/start' и '/hello'
+
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-    bot.reply_to(message, """\
-Привет, я эхобот, я буду отвечать также как и ты!\
-""")
-@bot.message_handler(func=lambda message: True)
-def echo_message(message):
-    bot.reply_to(message, message.text)
+    bot.reply_to(message, "Hi! Use /set <seconds> to set a timer")
 
-# Запуск бота
-bot.infinity_polling()
+
+def beep(chat_id) -> None:
+    """Send the beep message."""
+    bot.send_message(chat_id, text='Beep!')
+
+
+@bot.message_handler(commands=['set'])
+def set_timer(message):
+    args = message.text.split()
+    if len(args) > 1 and args[1].isdigit():
+        sec = int(args[1])
+        schedule.every(sec).seconds.do(beep, message.chat.id).tag(message.chat.id)
+    else:
+        bot.reply_to(message, 'Usage: /set <seconds>')
+
+
+@bot.message_handler(commands=['unset'])
+def unset_timer(message):
+    schedule.clear(message.chat.id)
+
+
+if __name__ == '__main__':
+    threading.Thread(target=bot.infinity_polling, name='bot_infinity_polling', daemon=True).start()
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
